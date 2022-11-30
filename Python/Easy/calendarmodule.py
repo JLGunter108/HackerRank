6620a9bfec625ee04ab7297fe017f668f80b800f
John Gunter <JLGunter108@gmail.com>
2022-11-30 12:06:18 -0800

    11/30/22 - 1 easy

---
+++ Python/Easy/calendarmodule.py
@@ -0,0 +1,9 @@
+import calendar
+
+days = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']
+
+m, d, y = map(int, input().split())
+
+day =  calendar.weekday(y,m,d)
+
+print(days[day])
